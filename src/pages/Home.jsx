fd359dee4e48b90f8049bcfe3de4869f79500d66
mary <marinadenis2018@gmail.com>
2026-06-08 21:28:01 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,7 +75,7 @@ function Home() {
 
   const sliderPets = [
     { src: '/mars1.png', id: 1 },
-    { src: '/bagir1.png', id: 2 },
+    { src: '/Karen1.png', id: 2 },
     { src: '/nensi1.png', id: 3 },
     { src: '/momo1.png', id: 4 },
     { src: '/kamila1.png', id: 5 }
